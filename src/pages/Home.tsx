4c6ba589ceb396ab18b8e2bc2e96bf20af82abe9
Tiffany <tiffanylouchao@gmail.com>
2022-04-11 17:28:07 -0400

    homepage hover

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Layout from 'layout/Layout'
 
 function Home() {
+
+    const [bgOpacity, setBgOpacity] = useState(.8333)
+
+    function handleMouseEnter(){
+        setBgOpacity(.625)
+    }
+
+    function handleMouseOut(){
+        setBgOpacity(.8333)
+    }
+
     return (
         <Layout>
             <div id="home">
+                <div id="home-title" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}>
+                    <Link to='/about'><p>Tiffany C</p></Link>
+                </div>
+                <div id="home-img-overlay">
+                    <div id="home-img" style={{ opacity: bgOpacity }}/>
+                </div>
             </div>
         </Layout>
     )
