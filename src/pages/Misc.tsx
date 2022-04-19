5a948111f4f936c9a7d80faa745440cbf8ff91a2
Tiffany <tiffanylouchao@gmail.com>
2022-04-19 10:26:05 -0400

    misc

--- src/pages/Misc.tsx
+++ src/pages/Misc.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import Layout from 'layout/Layout'
 
 const Misc = () => {
-    const art : string[] = ['vivo', 'cardinals', 'bugatti', 'sargent', 'surfaces']
+    const art : string[] = ['akira', 'vivo', 'cardinals', 'bugatti', 'sargent', 'surfaces']
     return (
         <Layout>
             <div id="misc">
-                {art.map((elem, idx) => 
-                    <img key={elem + '-' + idx}
-                         src={process.env.PUBLIC_URL + '/art/' + elem + '.png'}
-                         alt={elem + 'png'}/>)}
+                <div id="misc-wrapper">
+                    <p>Renders, and artwork completed for a workshop located in Pasadena, California.</p>
+                    <div className="img-container">
+                    {art.map((elem, idx) => 
+                        <img key={elem + '-' + idx}
+                            src={process.env.PUBLIC_URL + '/art/' + elem + '.png'}
+                            alt={elem + 'png'}/>)}
+                    </div>
+                </div>
             </div>
         </Layout>
     )
